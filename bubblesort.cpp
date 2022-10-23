38ed84a63e8f172116f1ca6c5431ba085997eb38
Tirtharaj <tirtharaj002@gmai.com>
2022-10-23 10:17:48 +0530

    Finding the maximum Prime divisor

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,37 +1,46 @@
-// C++ program for implementation
-// of Bubble sort
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
+void printArray(int *array, int n)
 {
-	int i, j;
-	for (i = 0; i < n - 1; i++)
-
-		// Last i elements are already
-		// in place
-		for (j = 0; j < n - i - 1; j++)
-			if (arr[j] > arr[j + 1])
-				swap(arr[j], arr[j + 1]);
+    for (int i = 0; i < n; ++i)
+        std::cout << array[i] << std::endl;
 }
 
-// Function to print an array
-void printArray(int arr[], int size)
+void bubbleSort(int *array, int n)
 {
-	int i;
-	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+    bool swapped = true;
+    int j = 0;
+    int temp;
+
+    while (swapped)
+    {
+        swapped = false;
+        j++;
+        for (int i = 0; i < n - j; ++i)
+        {
+            if (array[i] > array[i + 1])
+            {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    }
 }
 
-// Driver code
 int main()
 {
-	int arr[] = { 5, 1, 4, 2, 8};
-	int N = sizeof(arr) / sizeof(arr[0]);
-	bubbleSort(arr, N);
-	cout << "Sorted array: \n";
-	printArray(arr, N);
-	return 0;
+    int array[] = {95, 45, 48, 98, 485, 65, 54, 478, 1, 2325};
+    int n = sizeof(array)/sizeof(array[0]);
+
+    std::cout << "Before Bubble Sort :" << std::endl;
+    printArray(array, n);
+
+    bubbleSort(array, n);
+
+    std::cout << "After Bubble Sort :" << std::endl;
+    printArray(array, n);
+    return (0);
 }
+@anmolch24
